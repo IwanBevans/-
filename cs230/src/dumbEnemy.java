ba4bcc274758438b1dbebec61467e90e9290b5b0
BuildTools <unconfigured@null.spigotmc.org>
2019-11-21 16:31:42 +0000

    Updated dumbEnemy to follow player rather than move randomly

--- cs230/src/dumbEnemy.java
+++ cs230/src/dumbEnemy.java
@@ -9,6 +9,37 @@ public class dumbEnemy extends Enemy{
 	}
 	
 	public void move(Level level,Player player) {
+		int playerX = player.locationX;
+		int playerY = player.locationY;
+		
+		int difX = playerX - locationX;
+		int difY = playerY - locationY;
+		
+		
+		if (difX < difY) {
+			if (difY < 0 && level.tiles[locationX][locationY-1].isPassableEnemy) {
+				locationY = locationY - 1;
+			}
+			if (difY > 0 && level.tiles[locationX][locationY+1].isPassableEnemy) {
+				locationY = locationY + 1;
+			}
+		}
+		
+		if (difY < difX) {
+			if (difX > 0 && level.tiles[locationX+1][locationY].isPassableEnemy) {
+				locationX = locationX + 1;
+			}
+			if (difX < 0 && level.tiles[locationX-1][locationY].isPassableEnemy) {
+				locationX = locationX - 1;
+			}
+			
+		}
+	}
+	
+	
+	
+/*
+	public void move(Level level,Player player) {
 		double move = Math.round(Math.random() * 4);
 		if (move == 0 && level.tiles[locationX-1][locationY].isPassableEnemy) {
 			locationX = locationX - 1;
@@ -20,4 +51,6 @@ public class dumbEnemy extends Enemy{
 			locationY = locationY + 1;
 		}
 	}
+	
+*/
 }
